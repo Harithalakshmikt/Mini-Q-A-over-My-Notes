c4ba8b72e2dec177e93b5cc6cc759936ecf1ce1d
Haritha Lakshmi.K.T <115219300+Harithalakshmikt@users.noreply.github.com>
2026-07-06 14:21:05 +0530

    Change default max_new_tokens to 50
    Reduced the default maximum number of new tokens from 200 to 50 in the FlanGenerator class.

--- rag_backend/flan_generator.py
+++ rag_backend/flan_generator.py
@@ -1,7 +1,7 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
 class FlanGenerator:
-    def __init__(self, model_name: str = "google/flan-t5-base", max_new_tokens: int = 200):
+    def __init__(self, model_name: str = "google/flan-t5-base", max_new_tokens: int = 50):
         tok = AutoTokenizer.from_pretrained(model_name)
         model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
         self.pipe = pipeline("text-generation", model=model, tokenizer=tok, max_new_tokens=max_new_tokens)
